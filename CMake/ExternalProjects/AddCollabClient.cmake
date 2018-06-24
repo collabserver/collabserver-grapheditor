866f666d820b1f3ea3043546b2186c54687e4c69
geekymoose <constantin.masson@umontreal.ca>
2018-06-23 20:50:58 -0400

    Submodule: integrate new commits and little changes in CMake

--- CMake/ExternalProjects/AddCollabClient.cmake
+++ CMake/ExternalProjects/AddCollabClient.cmake
@@ -3,15 +3,15 @@
 # ------------------------------------------------------------------------------
 include(ExternalProject)
 ExternalProject_Add(collab-client-interface
-    SOURCE_DIR          "${git_submodule_dir}/collab-client-interface/"
+    SOURCE_DIR          "${collab_gitmodule_dir}/collab-client-interface/"
     BINARY_DIR          "${CMAKE_BINARY_DIR}/collab-client-interface-build"
     INSTALL_COMMAND     ""
     TEST_COMMAND        ""
 )
 
 ExternalProject_Get_Property(collab-client-interface binary_dir)
-add_library(client_interface STATIC IMPORTED)
-set_property(TARGET client_interface PROPERTY IMPORTED_LOCATION
-    "${binary_dir}/libclient_interface.a")
+add_library(collab_client STATIC IMPORTED)
+set_property(TARGET collab_client PROPERTY IMPORTED_LOCATION
+    "${binary_dir}/libcollab_client.a")
 
 
