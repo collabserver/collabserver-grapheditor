9ec12a28eda9c3ced83e028641cd51ba9991febc
geekymoose <constantin.masson@umontreal.ca>
2018-06-26 17:08:35 -0400

    Integrate new submodule commits and rename client/database folders

--- grapheditor/src/main.cpp
+++ grapheditor/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "collabdata/custom/SimpleGraph.h"
+
 int main(int argc, char** argv) {
     std::cout << "Debug: Client code executed\n";
+    collab::SimpleGraph graph;
     // TODO Client example goes here
     return 0;
 }
--- mvk/src/main.cpp
+++ mvk/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "collabdata/custom/SimpleGraph.h"
+
 int main(int argc, char** argv) {
     std::cout << "Debug: Database code executed\n";
+    collab::SimpleGraph graph;
     // TODO Database Mvk proxy goes here
     return 0;
 }
