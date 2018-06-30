8cb83ed21559282da0de498967a5d335e2144e77
geekymoose <constantin.masson@umontreal.ca>
2018-06-29 20:07:27 -0400

    Update README and fix error in CMake build

--- CMake/AddGraphiceditor.cmake
+++ CMake/AddGraphiceditor.cmake
@@ -7,7 +7,8 @@ include_directories("${collab_gitmodule_dir}/collab-client-interface/include")
 file(GLOB_RECURSE srcGrapheditor "${CMAKE_SOURCE_DIR}/grapheditor/src/*.cpp")
 
 add_executable(grapheditor_poc ${srcGrapheditor})
+add_dependencies(grapheditor_poc collab-data-crdts)
 add_dependencies(grapheditor_poc collab-client-interface)
 add_custom_target(runGrapheditor grapheditor_poc)
 
-target_link_libraries(grapheditor_poc collab_client)
+target_link_libraries(grapheditor_poc collab_client collabdata)
